aa76e4e390df432e15c0de4b4da3840bc691d869
Sudhakar-sk <57562716+Sudhakar-sk@users.noreply.github.com>
2021-04-03 18:50:45 +0530

    Initialize master

---
+++ config.php
@@ -0,0 +1,8 @@
+
+
+<?php 
+$conn=mysqli_connect("127.0.0.1","root","");
+ mysqli_select_db($conn,'sk_bank');
+
+
+?>
---
+++ navbar.php
@@ -0,0 +1,29 @@
+<!--navbar-->
+ <nav class="navbar navbar-expand-md sticky-top bg-dark navbar-dark">
+      <a class="navbar-brand" href="index.php">SK_BANK</a>
+   
+   <button class="navbar-toggler"type="button"data-toggle="collapse" data-taget="#coapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+   </button>
+   
+   <div class="collapse navbar-collapse" id="collapsibleNavbar">
+   
+     <ul class="navbar-nav ml-auto">
+	    <li class="nav-item">
+		  <a class="nav-link navbar-danger" href="index.php">Home</a>
+		 </li>
+         
+		 <li class="nav-item">
+		    <a class="nav-link navbar-danger" href="createuser.php">create user</a>
+		  </li>
+		 
+		<li class="nav-item">
+		  <a class="nav-link" href="transfermoney.php">Transfer Money</a>
+		 </li>
+		 
+		<li class="nav-item">
+		  <a class="nav-link" href="transactionhistory.php">Transaction history</a>
+		</li>
+	 </ul>
+	</div>
+   </nav>
